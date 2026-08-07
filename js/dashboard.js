b55cc68b07c522e71c1e6126676d049e51fc9cc9
chardpar-git <chardpar@gmail.com>
2026-08-07 09:10:12 +0800

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,16 @@
+const voucher = localStorage.getItem("voucher");
 
+if(!voucher){
+    window.location="index.html";
+}
+
+document.getElementById("welcome").innerHTML =
+"Welcome<br><small class='text-muted'>Voucher : "+voucher+"</small>";
+
+function logout(){
+
+localStorage.removeItem("voucher");
+
+window.location="index.html";
+
+}
