8820cf5354ccc08ad7a392b92daad1f885e42ef8
chardpar-git <chardpar@gmail.com>
2026-08-07 08:51:28 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
+function login(){
 
+const voucher=document.getElementById("voucher").value;
+
+if(voucher===""){
+
+alert("Please enter your voucher.");
+
+return;
+
+}
+
+localStorage.setItem("voucher",voucher);
+
+window.location="dashboard.html";
+
+}
